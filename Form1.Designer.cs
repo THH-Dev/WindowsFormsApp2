dbac0caefbfcb36c5dc2abd1feea1636c6cb3111
John Nguyen <tuanken92@gmail.com>
2023-02-21 14:34:28 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cogAcqFifoEditV21 = new Cognex.VisionPro.CogAcqFifoEditV2();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnLoad4Img = new System.Windows.Forms.Button();
+            this.btnStitchImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.cogAcqFifoEditV21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cogAcqFifoEditV21
+            // 
+            this.cogAcqFifoEditV21.Location = new System.Drawing.Point(3, 3);
+            this.cogAcqFifoEditV21.MinimumSize = new System.Drawing.Size(489, 0);
+            this.cogAcqFifoEditV21.Name = "cogAcqFifoEditV21";
+            this.cogAcqFifoEditV21.Size = new System.Drawing.Size(835, 445);
+            this.cogAcqFifoEditV21.SuspendElectricRuns = false;
+            this.cogAcqFifoEditV21.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 494);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnLoad4Img
+            // 
+            this.btnLoad4Img.Location = new System.Drawing.Point(117, 494);
+            this.btnLoad4Img.Name = "btnLoad4Img";
+            this.btnLoad4Img.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad4Img.TabIndex = 1;
+            this.btnLoad4Img.Text = "Load4Image";
+            this.btnLoad4Img.UseVisualStyleBackColor = true;
+            this.btnLoad4Img.Click += new System.EventHandler(this.btnLoad4Img_Click);
+            // 
+            // btnStitchImage
+            // 
+            this.btnStitchImage.Location = new System.Drawing.Point(228, 494);
+            this.btnStitchImage.Name = "btnStitchImage";
+            this.btnStitchImage.Size = new System.Drawing.Size(75, 23);
+            this.btnStitchImage.TabIndex = 1;
+            this.btnStitchImage.Text = "Stitch";
+            this.btnStitchImage.UseVisualStyleBackColor = true;
+            this.btnStitchImage.Click += new System.EventHandler(this.btnStitchImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 551);
+            this.Controls.Add(this.btnStitchImage);
+            this.Controls.Add(this.btnLoad4Img);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.cogAcqFifoEditV21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cogAcqFifoEditV21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Cognex.VisionPro.CogAcqFifoEditV2 cogAcqFifoEditV21;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnLoad4Img;
+        private System.Windows.Forms.Button btnStitchImage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using Cognex.VisionPro;
+using Cognex.VisionPro.Display;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Markup;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        CogAcqFifoTool AcqFifoTool;
+        CogImage8Grey cogImage8Grey;
+        string fileVpp = @"C:\Users\Admin\Desktop\line-scan\AcqFifo.vpp";
+        Mat mat;
+        object lockObj = new object();
+        CogImageStitch cogImageStitch;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sendLog(string log)
+        {
+            Console.WriteLine(log);
+        }
+
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (mat != null && mat.Size().Width > 0)
+            Cv2.ImShow("hi", mat);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Set reference to CogAcqFifoTool created by Edit Control
+            //The Acq Fifo Edit Control creates its subject when its AutoCreateTool property is True
+            AcqFifoTool = CogSerializer.LoadObjectFromFile(fileVpp) as CogAcqFifoTool;
+            AcqFifoTool = cogAcqFifoEditV21.Subject;
+            AcqFifoTool.Changed += AcqFifoTool_Changed;
+            AcqFifoTool.Ran += AcqFifoTool_Ran;
+            //Operator will be Nothing if no Frame Grabber is available.  Disable the Frame Grabber
+            //option on the "VisionPro Demo" tab if no frame grabber available.
+            if (AcqFifoTool.Operator == null)
+            {
+                MessageBox.Show("AcqFifoTool.Operator");
+            }
+
+        }
+
+        private void AcqFifoTool_Changed(object sender, CogChangedEventArgs e)
+        {
+            sendLog("AcqFifoTool_Changed");
+        }
+
+        int static_AcqFifoTool_Ran_numacqs = 0;
+        bool bIsFirstImg = false;
+        private void AcqFifoTool_Ran(object sender, EventArgs e)
+        {
+            sendLog("AcqFifoTool_Ran");
+            lock(lockObj)
+            {
+                cogImage8Grey = AcqFifoTool.OutputImage as CogImage8Grey;
+                if (cogImage8Grey == null)
+                    return;
+
+                
+                //decode 2
+                Bitmap bitmap = cogImage8Grey.ToBitmap();
+                Mat temp = bitmap.ToMat();
+                byte[] data = temp.ToBytes();
+                Mat src2 = new Mat(256, 4096, MatType.CV_8UC1);
+                src2.SetArray(data);
+                if (!src2.Empty())
+                {
+                    //Console.WriteLine("Mat SizeX: {0}", src2.Size().Width);
+                    //Console.WriteLine("Mat SizeY: {0}", src2.Size().Height);
+                    //Console.WriteLine("Mat Channels: {0}", src2.Channels());
+
+                    if (!bIsFirstImg)
+                    {
+                        bIsFirstImg = true;
+                        mat = src2.Clone();
+                    }
+                    else
+                    {
+                        Cv2.VConcat(mat, src2, mat);
+                    }
+                    src2.Release();
+                }
+            }
+
+
+            // Run the garbage collector to free unused images
+            static_AcqFifoTool_Ran_numacqs += 1;
+            if (static_AcqFifoTool_Ran_numacqs > 4)
+            {
+                GC.Collect();
+                static_AcqFifoTool_Ran_numacqs = 0;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            AcqFifoTool.Changed += AcqFifoTool_Changed;
+            AcqFifoTool.Ran += AcqFifoTool_Ran;
+            AcqFifoTool.Dispose();
+        }
+
+        string img1 = @"G:\Program\_example\ImageStitchTest_V2Controls\ImageStitchTest_V2Controls\4Cam Test\Cam OL.tif";
+        string img2 = @"G:\Program\_example\ImageStitchTest_V2Controls\ImageStitchTest_V2Controls\4Cam Test\Cam OR.tif";
+        string img3 = @"G:\Program\_example\ImageStitchTest_V2Controls\ImageStitchTest_V2Controls\4Cam Test\Cam UL.tif";
+        string img4 = @"G:\Program\_example\ImageStitchTest_V2Controls\ImageStitchTest_V2Controls\4Cam Test\Cam UR.tif";
+        List<CogImage8Grey> listCogImage = new List<CogImage8Grey>();
+        private void btnLoad4Img_Click(object sender, EventArgs e)
+        {
+            
+            Bitmap bitmap1 = new Bitmap(img1);
+            Bitmap bitmap2 = new Bitmap(img2);
+            Bitmap bitmap3 = new Bitmap(img3);
+            Bitmap bitmap4 = new Bitmap(img4);
+
+            List<Bitmap> listImage = new List<Bitmap>(); 
+            listImage.Add(bitmap1);
+            listImage.Add(bitmap2);
+            listImage.Add(bitmap3);
+            listImage.Add(bitmap4);
+
+            
+            foreach(Bitmap bitmap in listImage)
+            {
+                CogImage8Grey cogImage = new CogImage8Grey(bitmap);
+                listCogImage.Add(cogImage);
+            }
+
+
+
+        }
+
+        private void btnStitchImage_Click(object sender, EventArgs e)
+        {
+            // Get the data for the new image
+            Int32 height = 950;
+            Int32 width = 1250;
+            CogImageStitch imgStitch = new CogImageStitch();
+            CogTransform2DLinear trans = new CogTransform2DLinear();
+
+            // Allocate the blending buffer
+            imgStitch.AllocateBlendingBuffer(width, height, trans);
+
+
+        }
+    }
+}
